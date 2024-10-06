0be1dfb08d93fbbd05ecc8760173d471eb386c70
Vlad <02kvg03@mail.ru>
2024-10-06 19:32:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AuthPage } from "./pages/auth-page";
+
+function App() {
+  return (
+    <>
+      <AuthPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/auth-page/auth-form/auth-form.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import "./auth-form.css";
+
+export const AuthForm: React.FC = () => {
+  return (
+    <div className="auth-form">
+      <Typography variant="h4">
+        h1. Heading
+      </Typography>
+    </div>
+  );
+};
